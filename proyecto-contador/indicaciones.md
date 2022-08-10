faa7b76549d9d77fb9b6938a108fa15109ff704e
Talia-Rodrigues <101725864+TaliaRodrigues22@users.noreply.github.com>
2022-08-10 18:22:13 -0300

    actualizacion

---
+++ proyecto-contador/indicaciones.md
@@ -0,0 +1,5 @@
+npm i -S react-redux
+npm i -D redux-devtools
+
+npm 
+npm install @reduxjs/toolkit
---
+++ proyecto-contador/src/components/Contador.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+// Declaro el cantodor 
+const Contador = ({ valor }) => {
+    return (
+        <div>Contador : {valor}</div>
+    )
+}
+// exporto
+export default Contador
